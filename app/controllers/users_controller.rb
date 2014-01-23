185da77150d48f6d3ae28f7de6275501ccb6a408
Milind <milind@hashcooki.es>
2014-01-23 12:29:56 +0530

    More styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 		if @user.save
 			flash[:success] = "Welcome to new user"
+			sign_in @user
 			redirect_to @user
 		else
 			render 'new'
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,8 @@
 class Client < ActiveRecord::Base
-	attr_accessible :name, :address, :user_id
+	attr_accessible :name, :address, :user_id, :email
 	has_many :invoices
 	belongs_to :user
+	
+	validates_presence_of :name
+	validates_presence_of :email
 end
